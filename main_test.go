e22053688dad3e7a3085286b09143a9405820af9
asadbek280604 <tozhimukhammedov.aa@phystech.edu>
2022-07-13 18:49:54 +0300

    add create/delete/update api tests

--- main_test.go
+++ main_test.go
@@ -8,34 +8,94 @@ import (
 	"testing"
 )
 
-func TestWork(t *testing.T) {
+func handelRequest(w *httptest.ResponseRecorder, request *http.Request) {
 	router := getRouter()
+	router.ServeHTTP(w, request)
+}
+
+func TestWork(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/albums", strings.NewReader(""))
 	w := httptest.NewRecorder()
-	router.ServeHTTP(w, request)
+	handelRequest(w, request)
 	if w.Code != http.StatusOK {
-		t.Fatal("status not ok")
+		t.Fatal("status must be ok")
 	}
 }
 
 func TestAlbumDetail(t *testing.T) {
-	router := getRouter()
 	albumID := 3
 	request, _ := http.NewRequest("GET", fmt.Sprintf("/albums/%d", albumID), strings.NewReader(""))
 	w := httptest.NewRecorder()
-	router.ServeHTTP(w, request)
+	handelRequest(w, request)
 	if w.Code != http.StatusOK {
-		t.Fatal("status not ok")
+		t.Fatal("status must be ok")
 	}
 }
 
 func TestAlbumNotFound(t *testing.T) {
-	router := getRouter()
 	albumID := 999
 	request, _ := http.NewRequest("GET", fmt.Sprintf("/albums/%d", albumID), strings.NewReader(""))
 	w := httptest.NewRecorder()
-	router.ServeHTTP(w, request)
+	handelRequest(w, request)
 	if w.Code != http.StatusNotFound {
-		t.Fatal("must be 404")
+		t.Fatal("status must be 404")
+	}
+}
+
+func TestDeleteAlbum(t *testing.T) {
+	albumID := 1
+	request, _ := http.NewRequest("DELETE", fmt.Sprintf("/albums/%d", albumID), strings.NewReader(""))
+	w := httptest.NewRecorder()
+	handelRequest(w, request)
+	if w.Code != http.StatusNoContent {
+		t.Fatal("status must be 204")
+	}
+}
+
+func TestDeleteAlbumNotFound(t *testing.T) {
+	albumID := 999
+	request, _ := http.NewRequest("DELETE", fmt.Sprintf("/albums/%d", albumID), strings.NewReader(""))
+	w := httptest.NewRecorder()
+	handelRequest(w, request)
+	if w.Code != http.StatusNotFound {
+		t.Fatal("status must be 404")
+	}
+}
+
+func TestUpdateAlbumNotFound(t *testing.T) {
+	albumID := 999
+	request, _ := http.NewRequest("PUT", fmt.Sprintf("/albums/%d", albumID), strings.NewReader(""))
+	w := httptest.NewRecorder()
+	handelRequest(w, request)
+	if w.Code != http.StatusNotFound {
+		t.Fatal("status must be 404")
+	}
+}
+
+func TestUpdateAlbum(t *testing.T) {
+	albumID := 2
+	request, _ := http.NewRequest("PUT", fmt.Sprintf("/albums/%d", albumID), strings.NewReader(`{"title": "test"}"`))
+	w := httptest.NewRecorder()
+	handelRequest(w, request)
+	if w.Code != http.StatusOK {
+		t.Fatal("status must be ok")
+	}
+}
+
+func TestCreateBadStorage(t *testing.T) {
+	request, _ := http.NewRequest("POST", "/albums", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	handelRequest(w, request)
+	if w.Code != http.StatusBadRequest {
+		t.Fatal("status must be 400", w.Code)
+	}
+}
+
+func TestCreateAlbum(t *testing.T) {
+	request, _ := http.NewRequest("POST", "/albums", strings.NewReader(`{"id": "4", "title": "a", "artist": "asd", "price": 49.99}`))
+	w := httptest.NewRecorder()
+	handelRequest(w, request)
+	if w.Code != http.StatusCreated {
+		t.Fatal("status must be 201", w.Code)
 	}
 }
