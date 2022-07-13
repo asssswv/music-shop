87afc832c347f8ff51fb20441a2872d69697922c
asadbek280604 <tozhimukhammedov.aa@phystech.edu>
2022-07-13 16:06:19 +0300

    fixed

--- data.go
+++ data.go
@@ -1,5 +1,12 @@
 package main
 
+type Album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
 var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry", Price: 17.99},
--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
 	"net/http"
 )
 
-type Album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
@@ -64,14 +57,18 @@ func updateAlbumByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"massage": "not_found"})
 }
 
-func main() {
+func getRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.PUT("/albums/:id", updateAlbumByID)
 	router.DELETE("/albums/:id", deleteAlbumByID)
 	router.POST("/albums", postAlbum)
+	return router
+}
 
+func main() {
+	router := getRouter()
 	err := router.Run("localhost:8080")
 	if err != nil {
 		return
